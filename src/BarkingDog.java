f2392d1bf7c5856d5abdc9d6a0a57b0ce4c08188
Dominik Fínik <92373597+FinikDominik@users.noreply.github.com>
2022-07-27 19:50:44 +0200

    Update BarkingDog.java

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,3 +1,12 @@
-public class shouldWakeUp {
-    
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean Barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23 || hourOfDay == 22 || hourOfDay == 22) {
+            System.out.println("false, " + hourOfDay);
+            return false;
+        }
+        System.out.println("true, " + hourOfDay);
+        return Barking;
+
+    }
 }
